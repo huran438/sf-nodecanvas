b95e37514a54f294497fda7973e33d2048db611d
i.khurani <huran438@gmail.com>
2023-03-07 01:10:21 +0300

    Improved Serialization

--- Editor/SFIdNodeCanvasAttributeDrawer.cs
+++ Editor/SFIdNodeCanvasAttributeDrawer.cs
@@ -14,25 +14,30 @@ namespace SFramework.NodeCanvas.Editor
 {
     public class SFIdNodeCanvasAttributeDrawer : AttributeDrawer<SFIdNodeCanvasAttribute>
     {
+        private int hash;
         private HashSet<ISFRepository> _repositories = new();
 
-        private bool CheckAndLoadDatabase(Type type)
+        private bool CheckAndLoadDatabase(Type type, object instance)
         {
-            if (_repositories.Count != 0) return true;
-            _repositories = SFEditorExtensions.FindRepositories(type); 
+            if (instance == null) return false;
+            if (instance.GetHashCode() == hash && _repositories.Count != 0) return true;
+            _repositories = SFEditorExtensions.FindRepositories(type);
+            hash = instance.GetHashCode();
             return _repositories.Count != 0;
         }
 
 
         public override object OnGUI(GUIContent content, object instance)
         {
+            if (instance == null) return string.Empty;
             if (fieldInfo.FieldType != typeof(string)) return MoveNextDrawer();
+
 
             var value = (string)instance;
 
             var sfTypeAttribute =
                 fieldInfo.GetCustomAttributes(typeof(SFIdNodeCanvasAttribute), false)[0] as SFIdNodeCanvasAttribute;
-            if (!CheckAndLoadDatabase(sfTypeAttribute.Type)) return MoveNextDrawer();
+            if (!CheckAndLoadDatabase(sfTypeAttribute.Type, instance)) return MoveNextDrawer();
 
             if (string.IsNullOrWhiteSpace(value))
             {
@@ -44,7 +49,7 @@ namespace SFramework.NodeCanvas.Editor
             foreach (var repository in _repositories)
             {
                 repository.Nodes.FindAllPaths(out var ids, sfTypeAttribute.Indent);
-                
+
                 foreach (var id in ids)
                 {
                     paths.Add($"{repository._Name}/{id}");
