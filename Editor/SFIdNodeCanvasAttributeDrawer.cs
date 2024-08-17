019a23abce1979562be65b94db4313734ef14365
i.khurani <huran438@gmail.com>
2024-08-18 01:32:40 +0300

    bump

--- Editor/SFIdNodeCanvasAttributeDrawer.cs
+++ Editor/SFIdNodeCanvasAttributeDrawer.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using ParadoxNotion.Design;
 using SFramework.Configs.Editor;
-using SFramework.Configs.Runtime;
 using SFramework.NodeCanvas.Runtime;
 using UnityEditor;
 using UnityEngine;
@@ -12,62 +10,23 @@ namespace SFramework.NodeCanvas.Editor
 {
     public class SFIdNodeCanvasAttributeDrawer : AttributeDrawer<SFIdNodeCanvasAttribute>
     {
-        private int _hash;
-        private readonly HashSet<ISFNodesConfig> _configs = new();
-        private readonly List<string> _paths = new List<string>();
-
-        private bool CheckAndLoadDatabase(Type type, object inst)
-        {
-            if (inst == null) return false;
-            if (inst.GetHashCode() == _hash && _configs.Count != 0) return true;
-            
-            foreach (var config in SFConfigsEditorExtensions.FindConfigs<ISFNodesConfig>(type))
-            {
-                _configs.Add(config);
-            }
-            
-            _hash = inst.GetHashCode();
-            return _configs.Count != 0;
-        }
-
-
         public override object OnGUI(GUIContent cont, object inst)
         {
             if (inst == null) return string.Empty;
             if (fieldInfo.FieldType != typeof(string)) return MoveNextDrawer();
-
 
             var value = (string)inst;
 
             var sfTypeAttribute = fieldInfo.GetCustomAttributes(typeof(SFIdNodeCanvasAttribute), false)[0] as SFIdNodeCanvasAttribute;
 
             if (sfTypeAttribute == null) return MoveNextDrawer();
-            
-            if (!CheckAndLoadDatabase(sfTypeAttribute.Type, inst)) return MoveNextDrawer();
+
+            var _paths = SFConfigsEditorExtensions.GetPaths(sfTypeAttribute.Type.Name, sfTypeAttribute.Indent);
+            if (_paths == null || _paths.Length == 0) return MoveNextDrawer();
 
             if (string.IsNullOrWhiteSpace(value))
             {
                 value = string.Empty;
-            }
-
-            _paths.Clear();
-            _paths.Add("-");
-
-            foreach (var config in _configs)
-            {
-                config.Children.FindAllPaths(out var ids, sfTypeAttribute.Indent);
-                
-                if (sfTypeAttribute.Indent == 0)
-                {
-                    _paths.Add(config.Id);
-                }
-                else
-                {
-                    foreach (var id in ids)
-                    {
-                        _paths.Add(string.Join("/", config.Id, id));
-                    }
-                }
             }
 
             if (!string.IsNullOrWhiteSpace(value) && !_paths.Contains(value))
@@ -79,18 +38,18 @@ namespace SFramework.NodeCanvas.Editor
                 ? value
                 : _paths[0];
 
-            var index = _paths.IndexOf(name);
+            var index = Array.IndexOf(_paths, name);
 
             if (index == 0)
             {
                 GUI.backgroundColor = Color.red;
             }
 
-            index = EditorGUILayout.Popup(cont, index, _paths.ToArray());
+            index = EditorGUILayout.Popup(cont, index, _paths);
 
             GUI.backgroundColor = Color.white;
 
-            return index == 0 ? string.Empty : _paths.ElementAt(index);
+            return index == 0 ? string.Empty : _paths[index];
         }
     }
 }
