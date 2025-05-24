8d64801ccff6cf258e351412b5682376a80c8d74
i.khurani <huran438@gmail.com>
2025-05-25 01:23:34 +0300

    Naming refactoring

--- Editor/SFIdNodeCanvasAttributeDrawer.cs
+++ Editor/SFIdNodeCanvasAttributeDrawer.cs
@@ -21,7 +21,7 @@ namespace SFramework.NodeCanvas.Editor
 
             if (sfTypeAttribute == null) return MoveNextDrawer();
 
-            var _paths = SFConfigsEditorExtensions.GetPaths(sfTypeAttribute.Type.Name, sfTypeAttribute.Indent);
+            var _paths = SFConfigsEditorUtility.GetNodePaths(sfTypeAttribute.Type.Name, sfTypeAttribute.Indent);
             if (_paths == null || _paths.Length == 0) return MoveNextDrawer();
 
             if (string.IsNullOrWhiteSpace(value))
